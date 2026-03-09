34b680edb975269e9b8e3f229beb968a6e288ac4
Flavia Kosti <flaviakosti@gmail.com>
2026-03-08 21:31:18 -0400

    Calendar P4

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -1,16 +1,37 @@
-import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, input, model, output } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DAYS } from '../../shared/constants/constants';
+import { UtilsService } from '../../shared/services/utils';
 
 @Component({
   selector: 'app-calendar',
-  imports: [MatDatepickerModule],
+  imports: [MatDatepickerModule, DatePipe],
   templateUrl: './calendar.html',
   styleUrl: './calendar.scss',
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Calendar {
+  private _utilsService = inject(UtilsService);
   selected = model<Date | null>(null);
+  public calendarDays = computed(() => this._calculateWeekDates());
+  public days: string[] = DAYS;
+  public currentDay: Date = new Date();
+  public activeDate = input<Date>(new Date());
+  public selectDate = output<Date>();
+
+  public updateCurrentMonth(move: number): void {
+    this.selectDate.emit(new Date(this.activeDate().getFullYear(), this.activeDate().getMonth() + move, 1));
+  }
+
+  public emitSelection(newDate: Date): void {
+    this.selectDate.emit(new Date(`${newDate}T12:00:00Z`));
+  }
+
+  private _calculateWeekDates(): string[] {
+    return this._utilsService.getDaysInMonth(this.activeDate().getFullYear(), this.activeDate().getMonth());
+  }
 }
 
---
+++ src/shared/services/utils.ts
@@ -0,0 +1,38 @@
+import { formatDate } from '@angular/common';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+	providedIn: 'root',
+})
+export class UtilsService {
+	public getDaysInMonth(year: number, month: number): string[] {
+		const numDays = new Date(year, month + 1, 0).getDate();
+		const daysArray = [];
+		const firstDayOfMonth = new Date(year, month, 1).getDay();
+		const daysFromPrevMonth = (firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1);
+		const lastDayOfPrevMonth = new Date(year, month, 0).getDate();
+		for (let i = lastDayOfPrevMonth - daysFromPrevMonth; i <= lastDayOfPrevMonth; i++) {
+			daysArray.push(formatDate(new Date(year, month - 1, i), 'yyyy-MM-dd', 'en-US'));
+		}
+
+		for (let day = 1; day <= numDays; day++) {
+			daysArray.push(formatDate(new Date(year, month, day), 'yyyy-MM-dd', 'en-US'));
+		}
+
+		const lastDayOfMonth = new Date(year, month + 1, 0).getDay();
+		const daysFromNextMonth = (lastDayOfMonth === 6 ? 0 : 6 - lastDayOfMonth);
+		for (let i = 1; i <= daysFromNextMonth; i++) {
+			daysArray.push(formatDate(new Date(year, month + 1, i), 'yyyy-MM-dd', 'en-US'));
+		}
+		return daysArray;
+	}
+
+
+	public generateRandomNumber(min: number, max: number): number {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+}
