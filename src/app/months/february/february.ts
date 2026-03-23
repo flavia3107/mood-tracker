214593a41cd43a39cecad380cbcb1b7956f72b48
Flavia Kosti <flaviakosti@gmail.com>
2026-03-23 19:44:30 -0400

    February - Config update P5

--- src/app/months/february/february.ts
+++ src/app/months/february/february.ts
@@ -26,7 +26,7 @@ export class February {
 
     // // Row 2: Large Squares
     { id: 4, pts: "M180,150 h70 v50 h-70 Z", cx: 210, cy: 180, color: '#fff', rotate: 55 },
-    { id: 5, pts: "M380,200 h80 v60 h-80 Z", cx: 415, cy: 235, color: '#fff', rotate: -6 },
+    { id: 5, pts: "M380,220 h80 v50 h-80 Z", cx: 415, cy: 235, color: '#fff', rotate: -6 },
     { id: 6, pts: "M240,225 h50 v70 h-50 Z", cx: 265, cy: 260, color: '#fff', rotate: 15 },
     { id: 7, pts: "M310,255 h50 v50 h-50 Z", cx: 335, cy: 280, color: '#fff', rotate: -3 },
     { id: 8, pts: "M390,285 h70 v70 h-70 Z", cx: 425, cy: 325, color: '#fff', rotate: 25 },
@@ -42,20 +42,20 @@ export class February {
     { id: 13, pts: "M135,235 h60 v60 h-60 Z", cx: 160, cy: 275, color: '#fff', rotate: 10 },
     // { id: 26, pts: "M205,385 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 230, cy: 410, color: '#fff', rotate: -9 },
     { id: 14, pts: "M115,350 h70 v50 h-70 Z", cx: 140, cy: 380, color: '#fff', rotate: 50 },
-    { id: 15, pts: "M280,325 h50 v50 h-50 Z", cx: 305, cy: 350, color: '#fff', rotate: -4 },
+    { id: 21, pts: "M280,325 h50 v50 h-50 Z", cx: 305, cy: 350, color: '#fff', rotate: -4 },
     // { id: 27, pts: "M345,385 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 370, cy: 410, color: '#fff', rotate: 11 },
-    // { id: 16, pts: "M370,370 h50 v50 h-50 Z", cx: 395, cy: 395, color: '#fff', rotate: -7 },
+    { id: 16, pts: "M320,395 h50 v50 h-50 Z", cx: 340, cy: 420, color: '#fff', rotate: 55 },
 
     // // Row 5: Bottom V-Shape
-    // { id: 17, pts: "M175,440 a30,15 0 1,1 60,0 a30,15 0 1,1 -60,0", cx: 205, cy: 455, color: '#fff', rotate: -10 },
-    // { id: 18, pts: "M250,425 a30,15 0 1,1 60,0 a30,15 0 1,1 -60,0", cx: 280, cy: 440, color: '#fff', rotate: 5 },
-    // { id: 19, pts: "M325,440 a30,15 0 1,1 60,0 a30,15 0 1,1 -60,0", cx: 355, cy: 455, color: '#fff', rotate: 12 },
+    { id: 17, pts: "M200,375 a30,15 0 1,1 70,0 a30,15 0 1,1 -70,0", cx: 235, cy: 375, color: '#fff', rotate: -10 },
+    { id: 18, pts: "M240,410 a30,15 0 1,1 60,0 a30,15 0 1,1 -60,0", cx: 270, cy: 410, color: '#fff', rotate: 5 },
+    { id: 19, pts: "M145,445 a30,15 0 1,1 60,0 a30,15 0 1,1 -60,0", cx: 175, cy: 445, color: '#fff', rotate: 50 },
     // { id: 22, pts: "M250,480 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 275, cy: 505, color: '#fff', rotate: 0 },
 
     // // Fillers
-    // { id: 20, pts: "M90,165 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 115, cy: 190, color: '#fff', rotate: 15 },
-    // { id: 21, pts: "M360,165 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 385, cy: 190, color: '#fff', rotate: -15 },
-    // { id: 23, pts: "M205,190 h50 v50 h-50 Z", cx: 230, cy: 215, color: '#fff', rotate: 5 },
+    { id: 20, pts: "M190,425 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 210, cy: 430, color: '#fff', rotate: 15 },
+    { id: 15, pts: "M375,190 a25,25 0 1,1 50,0 a25,25 0 1,1 -50,0", cx: 400, cy: 190, color: '#fff', rotate: -15 },
+    { id: 23, pts: "M205,190 h50 v50 h-50 Z", cx: 230, cy: 215, color: '#fff', rotate: 5 },
     // { id: 24, pts: "M295,190 h50 v50 h-50 Z", cx: 320, cy: 215, color: '#fff', rotate: -5 }
   ]);
   updateMood(day: any) {
