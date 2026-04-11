b97ecc1a6d04e5f4841b43f857809afa4fada91e
Flavia Kosti <flaviakosti@gmail.com>
2026-04-10 21:35:24 -0400

    Code Clean Up P17

--- src/app/mood-view/mood-view.ts
+++ src/app/mood-view/mood-view.ts
@@ -1,3 +1,6 @@
+import { NgTemplateOutlet } from '@angular/common';
+import { computed, viewChild } from '@angular/core';
+import { TemplateRef } from '@angular/core';
 import { Component, inject } from '@angular/core';
 import { UtilsService } from '../../shared/services/utils';
 import { April } from '../months/april/april';
@@ -15,11 +18,60 @@ import { September } from '../months/september/september';
 
 @Component({
   selector: 'app-mood-view',
-  imports: [January, February, March, April, May, June, July, August, September, October, November, December],
+  imports: [January, February, March, April, May, June, July, August, September, October, November, December, NgTemplateOutlet],
   templateUrl: './mood-view.html',
   styleUrl: './mood-view.scss',
 })
 export class MoodView {
   private _utilService = inject(UtilsService);
   public currentMonth = this._utilService.activeMonth;
+  jan = viewChild<TemplateRef<any>>('january');
+  feb = viewChild<TemplateRef<any>>('february');
+  mar = viewChild<TemplateRef<any>>('march');
+  apr = viewChild<TemplateRef<any>>('april');
+  may = viewChild<TemplateRef<any>>('may');
+  jun = viewChild<TemplateRef<any>>('june');
+  jul = viewChild<TemplateRef<any>>('july');
+  aug = viewChild<TemplateRef<any>>('august');
+  sep = viewChild<TemplateRef<any>>('september');
+  oct = viewChild<TemplateRef<any>>('october');
+  nov = viewChild<TemplateRef<any>>('november');
+  dec = viewChild<TemplateRef<any>>('december');
+  moodLogic = {
+    // This wraps your color logic
+    getColor: (day: number) => this.getMoodColorForDate(day),
+
+    // This wraps your click/update logic
+    select: (day: number) => this.onDayClick(day)
+  };
+
+
+  // Use computed to create a reactive template selector
+  activeTemplate = computed(() => {
+    const map: Record<string, TemplateRef<any> | undefined> = {
+      'January': this.jan(),
+      'February': this.feb(),
+      'March': this.mar(),
+      'April': this.apr(),
+      'May': this.may(),
+      'June': this.jun(),
+      'July': this.jul(),
+      'August': this.aug(),
+      'September': this.sep(),
+      'October': this.oct(),
+      'November': this.nov(),
+      'December': this.dec(),
+    };
+
+    return map[this.currentMonth()];
+  });
+
+
+  private getMoodColorForDate(day: number) {
+    // Your logic to check if it's the past and return a random color
+  }
+
+  private onDayClick(day: number) {
+    // Your logic to open a modal or update the mood
+  }
 }
