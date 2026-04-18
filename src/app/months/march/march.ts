63fca899b4bb17c997cb18465689276b0edb7d36
Flavia Kosti <flaviakosti@gmail.com>
2026-04-17 22:03:17 -0400

    Code Clean Up P59

--- src/app/months/march/march.ts
+++ src/app/months/march/march.ts
@@ -21,10 +21,42 @@ export class March {
     "M40,20 L50,5 L60,20 Z", "M60,20 L50,55 L40,20 Z",
     "M90,65 L50,55 L60,20 Z", "M120,0 L90,65 L50,10 Z"
   ];
-  private _config = this._setUpConfig();
+  public config = [
+    { "day": 1, "color": "#fff", "path": "M50,100 L10,65 L50,55 Z", "leaf": 0, "x": 40, "y": 75 },
+    { "day": 2, "color": "#fff", "path": "M50,100 L50,55 L90,65 Z", "leaf": 0, "x": 60, "y": 75 },
+    { "day": 3, "color": "#fff", "path": "M0,70 L0,10 L90,15 Z", "leaf": 0, "x": 20, "y": 45 },
+    { "day": 4, "color": "#fff", "path": "M40,20 L50,55 L10,65 Z", "leaf": 0, "x": 35, "y": 45 },
+    { "day": 5, "color": "#fff", "path": "M40,20 L50,5 L60,20 Z", "leaf": 0, "x": 50, "y": 20 },
+    { "day": 6, "color": "#fff", "path": "M60,20 L50,55 L40,20 Z", "leaf": 0, "x": 50, "y": 40 },
+    { "day": 7, "color": "#fff", "path": "M90,65 L50,55 L60,20 Z", "leaf": 0, "x": 65, "y": 45 },
+    { "day": 8, "color": "#fff", "path": "M120,0 L90,65 L50,10 Z", "leaf": 0, "x": 80, "y": 45 },
+    { "day": 9, "color": "#fff", "path": "M50,100 L10,65 L50,55 Z", "leaf": 1, "x": 40, "y": 75 },
+    { "day": 10, "color": "#fff", "path": "M50,100 L50,55 L90,65 Z", "leaf": 1, "x": 60, "y": 75 },
+    { "day": 11, "color": "#fff", "path": "M0,70 L0,10 L90,15 Z", "leaf": 1, "x": 20, "y": 45 },
+    { "day": 12, "color": "#fff", "path": "M40,20 L50,55 L10,65 Z", "leaf": 1, "x": 35, "y": 45 },
+    { "day": 13, "color": "#fff", "path": "M40,20 L50,5 L60,20 Z", "leaf": 1, "x": 50, "y": 20 },
+    { "day": 14, "color": "#fff", "path": "M60,20 L50,55 L40,20 Z", "leaf": 1, "x": 50, "y": 40 },
+    { "day": 15, "color": "#fff", "path": "M90,65 L50,55 L60,20 Z", "leaf": 1, "x": 65, "y": 45 },
+    { "day": 16, "color": "#fff", "path": "M120,0 L90,65 L50,10 Z", "leaf": 1, "x": 80, "y": 45 },
+    { "day": 17, "color": "#fff", "path": "M50,100 L10,65 L50,55 Z", "leaf": 2, "x": 40, "y": 75 },
+    { "day": 18, "color": "#fff", "path": "M50,100 L50,55 L90,65 Z", "leaf": 2, "x": 60, "y": 75 },
+    { "day": 19, "color": "#fff", "path": "M0,70 L0,10 L90,15 Z", "leaf": 2, "x": 20, "y": 45 },
+    { "day": 20, "color": "#fff", "path": "M40,20 L50,55 L10,65 Z", "leaf": 2, "x": 35, "y": 45 },
+    { "day": 21, "color": "#fff", "path": "M40,20 L50,5 L60,20 Z", "leaf": 2, "x": 50, "y": 20 },
+    { "day": 22, "color": "#fff", "path": "M60,20 L50,55 L40,20 Z", "leaf": 2, "x": 50, "y": 40 },
+    { "day": 23, "color": "#fff", "path": "M90,65 L50,55 L60,20 Z", "leaf": 2, "x": 65, "y": 45 },
+    { "day": 24, "color": "#fff", "path": "M120,0 L90,65 L50,10 Z", "leaf": 2, "x": 80, "y": 45 },
+    { "day": 25, "color": "#fff", "path": "M50,100 L10,65 L50,55 Z", "leaf": 3, "x": 40, "y": 75 },
+    { "day": 26, "color": "#fff", "path": "M50,100 L50,55 L90,65 Z", "leaf": 3, "x": 60, "y": 75 },
+    { "day": 27, "color": "#fff", "path": "M0,70 L0,10 L90,15 Z", "leaf": 3, "x": 20, "y": 45 },
+    { "day": 28, "color": "#fff", "path": "M40,20 L50,55 L10,65 Z", "leaf": 3, "x": 35, "y": 45 },
+    { "day": 29, "color": "#fff", "path": "M40,20 L50,5 L60,20 Z", "leaf": 3, "x": 50, "y": 20 },
+    { "day": 30, "color": "#fff", "path": "M60,20 L50,55 L40,20 Z", "leaf": 3, "x": 50, "y": 40 },
+    { "day": 31, "color": "#fff", "path": "M60,15 C85,0 100,10 105,35 L90,65 L50,55 Z", "leaf": 3, "x": 65, "y": 45 }
+  ];
 
-  updateDay(index: number) {
-    console.log('HERE', this._config)
+  updateDay(index: any) {
+    console.log('HERE', this.config)
     const selectedDate = new Date(this._date().getFullYear(), this._date().getMonth(), index + 1)
     const d2 = new Date();
     selectedDate.setHours(0, 0, 0, 0);
@@ -34,54 +66,9 @@ export class March {
       this.days[index].color = this._selectedMood;
   }
 
-  getLeafTransform(leafIndex: number): string {
-    const rotations = [0, 90, 180, 270];
-    const gap = 3;
-
-    const offsets = [
-      { x: 150, y: 80 - gap },
-      { x: 220 + gap, y: 150 },
-      { x: 150, y: 220 + gap },
-      { x: 80 - gap, y: 150 }
-    ];
-
-    const pos = offsets[leafIndex];
-    return `translate(${pos.x}, ${pos.y}) scale(1.3) rotate(${rotations[leafIndex]}, 50, 50)`;
-  }
-
-  getLabelPos(shardIndex: number) {
-    const centers = [
-      { x: 40, y: 75 }, { x: 60, y: 75 },
-      { x: 20, y: 45 }, { x: 35, y: 45 },
-      { x: 50, y: 20 }, { x: 50, y: 40 },
-      { x: 65, y: 45 }, { x: 80, y: 45 }
-    ];
-    return centers[shardIndex];
-  }
 
   updateMood(color: string) {
     this._selectedMood = color;
   }
 
-  private _setUpConfig() {
-    const days: { day: number; color: string; path: string; leaf: number; x: number; y: number; }[] = [];
-    for (let i = 0; i < 4; i++) {
-      this.shardPaths.forEach((shard, index) => {
-        const dayIdx = (i * 8) + index;
-        const path = dayIdx === 30 ? 'M60,15 C85,0 100,10 105,35 L90,65 L50,55 Z' : shard;
-        const { x, y } = this.getLabelPos(index)
-        if (dayIdx < 31) {
-          days.push({
-            day: dayIdx + 1,
-            color: '#fff',
-            path,
-            leaf: i,
-            x,
-            y
-          })
-        }
-      });
-    }
-    return days;
-  }
 }
