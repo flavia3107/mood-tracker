b45b14476dd7b963ab3f3194e8adf7b4e0804454
Flavia Kosti <flaviakosti@gmail.com>
2026-05-28 21:17:59 -0400

    Month Details P8

--- src/app/month-details/month-details.ts
+++ src/app/month-details/month-details.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OtherStatistics } from './other-statistics/other-statistics';
 
 @Component({
   selector: 'app-month-details',
-  imports: [],
+  imports: [OtherStatistics],
   templateUrl: './month-details.html',
   styleUrl: './month-details.scss',
 })
-export class MonthDetails {}
+export class MonthDetails { }
--- src/app/month-details/other-statistics/other-statistics.ts
+++ src/app/month-details/other-statistics/other-statistics.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './other-statistics.html',
   styleUrl: './other-statistics.scss',
 })
-export class OtherStatistics {}
+export class OtherStatistics { }
