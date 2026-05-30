4c40daa917b92f7c4a1f26ba47bdfc52b038f314
Flavia Kosti <flaviakosti@gmail.com>
2026-05-30 18:44:36 -0400

    Average Feeling P1

--- src/app/month-details/average-feeling/average-feeling.ts
+++ src/app/month-details/average-feeling/average-feeling.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './average-feeling.html',
   styleUrl: './average-feeling.scss',
 })
-export class AverageFeeling {}
+export class AverageFeeling {
+  value: number = 7.42;
+
+  radius = 85;
+  circumference = 2 * Math.PI * this.radius; // Total length of the circle border (~534)
+  strokeDashoffset = this.circumference;
+
+  ngOnChanges(): void {
+    // Keep value between 0 and 100
+    const boundedValue = Math.min(Math.max(this.value, 0), 10);
+    // Calculate how much of the track to color
+    this.strokeDashoffset = this.circumference - (boundedValue / 10) * this.circumference;
+  }
+}
