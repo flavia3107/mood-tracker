2dcdd6afa355429f516d2e8c50dce914cd688664
Flavia Kosti <flaviakosti@gmail.com>
2026-06-02 22:05:25 -0400

    Average Feeling P6

--- src/app/month-details/average-feeling/average-feeling.ts
+++ src/app/month-details/average-feeling/average-feeling.ts
@@ -15,7 +15,6 @@ export class AverageFeeling {
   circumference = 2 * Math.PI * this.radius;
   strokeDashoffset = this.circumference;
 
-
   private _getAverage(): number {
     const monthConfig = this._utilsService.monthConfig().filter(month => month.color !== '#fff');
 
@@ -23,13 +22,11 @@ export class AverageFeeling {
       this.strokeDashoffset = this.circumference;
       return 0;
     }
-
     const totalSum = monthConfig.reduce((sum, current) => sum + (Number(current?.value) || 0), 0);
     const trueAverage = totalSum / monthConfig.length;
     const clampedAverage = Math.min(Math.max(trueAverage, 1), 6);
-    const scaledValue = ((clampedAverage - 1) / (6 - 1)) * (10 - 1) + 1;
-
-    this.strokeDashoffset = this.circumference - (scaledValue / 10) * this.circumference;
-    return trueAverage;
+    const scaledAverage = ((clampedAverage - 1) / (6 - 1)) * (10 - 1) + 1;
+    this.strokeDashoffset = this.circumference - (scaledAverage / 10) * this.circumference;
+    return scaledAverage;
   }
 }
