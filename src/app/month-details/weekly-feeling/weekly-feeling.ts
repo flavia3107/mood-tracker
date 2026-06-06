35053773fce51aba847681b41d6e541328ec2d9f
Flavia Kosti <flaviakosti@gmail.com>
2026-06-06 17:39:52 -0400

    Weekly Feeling P13

--- src/app/month-details/weekly-feeling/weekly-feeling.ts
+++ src/app/month-details/weekly-feeling/weekly-feeling.ts
@@ -1,30 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
-import { ApexAnnotations, ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexFill, ApexGrid, ApexLegend, ApexMarkers, ApexNonAxisChartSeries, ApexPlotOptions, ApexResponsive, ApexStates, ApexStroke, ApexTheme, ApexTitleSubtitle, ApexTooltip, ApexXAxis, ApexYAxis, NgApexchartsModule } from 'ng-apexcharts';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ChartOptions, CHART_OPTIONS } from '../../../shared/constants/chart-models';
 import { UtilsService } from '../../../shared/services/utils';
-
-export type ChartOptions = {
-  series?: ApexAxisChartSeries | ApexNonAxisChartSeries;
-  chart?: ApexChart;
-  xaxis?: ApexXAxis;
-  yaxis?: ApexYAxis | ApexYAxis[];
-  title?: ApexTitleSubtitle;
-  subtitle?: ApexTitleSubtitle;
-  dataLabels?: ApexDataLabels;
-  stroke?: ApexStroke;
-  fill?: ApexFill;
-  legend?: ApexLegend;
-  tooltip?: ApexTooltip;
-  markers?: ApexMarkers;
-  plotOptions?: ApexPlotOptions;
-  responsive?: ApexResponsive[];
-  grid?: ApexGrid;
-  annotations?: ApexAnnotations;
-  states?: ApexStates;
-  theme?: ApexTheme;
-  colors?: string[];
-  labels?: any;
-};
 
 @Component({
   selector: 'app-weekly-feeling',
@@ -37,50 +15,9 @@ export class WeeklyFeeling {
   private _datePipe = inject(DatePipe);
   private _utilService = inject(UtilsService);
   private _weekDates = this._utilService.currentWeek;
-
-  readonly monthConfig = this._utilService.monthConfig;
   public weekDays = computed(() => this._getWeekDays());
-
-  public chartOptions: Partial<ChartOptions> = {
-    series: [
-      {
-        data: this.weekDays()['weekConfig'].map((day: any) => day.value),
-      },
-    ],
-    chart: {
-      type: 'area',
-      height: 50,
-      width: 280,
-      zoom: {
-        enabled: false,
-      },
-      sparkline: {
-        enabled: true
-      }
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: 'smooth',
-    },
-    labels: this.weekDays()['weekConfig'].map((day: any) => new Date(day.date).toDateString()),
-    xaxis: {
-      type: 'datetime',
-      labels: {
-        show: false
-      },
-      axisBorder: {
-        show: false
-      },
-      axisTicks: {
-        show: false
-      }
-    },
-    yaxis: {
-      show: false
-    }
-  };
+  public chartOptions = computed(() => this._getChartConfig());
+  readonly monthConfig = this._utilService.monthConfig;
 
   private _getWeekDays() {
     const weekDates = this._weekDates();
@@ -95,5 +32,20 @@ export class WeeklyFeeling {
       isSameMonth: this._datePipe.transform(weekDates[0], 'MMM') === this._datePipe.transform(weekDates[6], 'MMM')
     }
     return monthConfig
+  }
+
+  private _getChartConfig() {
+    const config: Partial<ChartOptions> = {
+      ...CHART_OPTIONS,
+      series: [
+        {
+          data: this.weekDays()['weekConfig'].map((day: any) => day.value),
+        },
+      ],
+      colors: ['#3b82f6'],
+      labels: this.weekDays()['weekConfig'].map((day: any) => new Date(day.date).toDateString()),
+    };
+
+    return config;
   }
 }
---
+++ src/shared/constants/chart-models.ts
@@ -0,0 +1,82 @@
+import {
+	ApexAnnotations,
+	ApexAxisChartSeries,
+	ApexChart,
+	ApexDataLabels,
+	ApexFill, ApexGrid,
+	ApexLegend,
+	ApexMarkers,
+	ApexNonAxisChartSeries,
+	ApexPlotOptions,
+	ApexResponsive,
+	ApexStates,
+	ApexStroke,
+	ApexTheme,
+	ApexTitleSubtitle,
+	ApexTooltip,
+	ApexXAxis,
+	ApexYAxis
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+	series?: ApexAxisChartSeries | ApexNonAxisChartSeries;
+	chart?: ApexChart;
+	xaxis?: ApexXAxis;
+	yaxis?: ApexYAxis | ApexYAxis[];
+	title?: ApexTitleSubtitle;
+	subtitle?: ApexTitleSubtitle;
+	dataLabels?: ApexDataLabels;
+	stroke?: ApexStroke;
+	fill?: ApexFill;
+	legend?: ApexLegend;
+	tooltip?: ApexTooltip;
+	markers?: ApexMarkers;
+	plotOptions?: ApexPlotOptions;
+	responsive?: ApexResponsive[];
+	grid?: ApexGrid;
+	annotations?: ApexAnnotations;
+	states?: ApexStates;
+	theme?: ApexTheme;
+	colors?: string[];
+	labels?: any;
+};
+
+
+export const CHART_OPTIONS: Partial<ChartOptions> = {
+	chart: {
+		type: 'area',
+		height: 50,
+		width: 320,
+		zoom: {
+			enabled: false,
+		},
+		sparkline: {
+			enabled: true
+		}
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	stroke: {
+		curve: 'smooth',
+		width: 2
+	},
+	fill: {
+		type: 'gradient',
+		gradient: {
+			shadeIntensity: 1,
+			opacityFrom: 0.7,
+			opacityTo: 0.2,
+			stops: [0, 90, 100]
+		}
+	},
+	xaxis: {
+		type: 'datetime',
+		labels: { show: false },
+		axisBorder: { show: false },
+		axisTicks: { show: false }
+	},
+	yaxis: {
+		show: false
+	}
+};
