be75124a43511d7dae4dfa2998d4a6fc98212dd0
Flavia Kosti <flaviakosti@gmail.com>
2026-06-27 16:57:09 -0400

    Navigation P4

--- src/app/month-details/month-details.ts
+++ src/app/month-details/month-details.ts
@@ -4,11 +4,10 @@ import { MoodTrends } from './mood-trends/mood-trends';
 import { OtherStatistics } from './other-statistics/other-statistics';
 import { TodayFeeling } from './today-feeling/today-feeling';
 import { WeeklyFeeling } from './weekly-feeling/weekly-feeling';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-month-details',
-  imports: [OtherStatistics, TodayFeeling, AverageFeeling, WeeklyFeeling, MoodTrends, MatIconModule],
+  imports: [OtherStatistics, TodayFeeling, AverageFeeling, WeeklyFeeling, MoodTrends],
   templateUrl: './month-details.html',
   styleUrl: './month-details.scss',
 })
